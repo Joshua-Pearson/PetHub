cace4fbe48e9cdb4592a8b9de55ca6a0ea53793d
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-05-03 15:41:07 -0700

    Third Commit adding secrets and forcing ssl

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-PetHub::Application.config.secret_key_base = '90ad20cfdf8c3829c4f1a7cbb4e4fd15cc5f3f50455bb5b61338e4d45a1ab0c8fbc14d2677849540e6441bcd876e88b459331f7e5dda54e8cb1fdc1be96c109f'
+PetHub::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
