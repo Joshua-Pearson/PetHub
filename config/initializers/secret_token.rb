282e86cf1054b194e330aaa9372afb7ab683f9f2
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-05-03 15:03:10 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PetHub::Application.config.secret_key_base = '90ad20cfdf8c3829c4f1a7cbb4e4fd15cc5f3f50455bb5b61338e4d45a1ab0c8fbc14d2677849540e6441bcd876e88b459331f7e5dda54e8cb1fdc1be96c109f'
