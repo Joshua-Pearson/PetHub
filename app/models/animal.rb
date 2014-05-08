b792b5216e62436d7927f7291d93fdd7059c0935
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-05-08 12:55:31 -0700

    Adding Css, still need to finish Css and rspec

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -7,15 +7,8 @@ class Animal < ActiveRecord::Base
   validates :breed, presence: true, length: {minimum: 2, maximum: 40}
   validates :age, presence: true, length: {minimum: 1, maximum: 20}
 
-  def self.animals
-    @animals = Animal.all
-  end
-
   def self.yesterday
     where('created_at BETWEEN ? AND ?', Date.yesterday.beginning_of_day, Time.zone.now.beginning_of_day).order('created_at desc')
-  end
-
-  def self.animals_this_past_week
   end
 
   def self.today
@@ -25,6 +18,7 @@ class Animal < ActiveRecord::Base
   def self.this_week
     where('created_at BETWEEN ? AND ?', 7.days.ago, 2.days.ago)
   end
+  
   def self.my_order
     order('created_at desc')
   end
